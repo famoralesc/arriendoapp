f5a7d4d0a9577237a8236bd4388b00557503e42c
Felipe Morales C <fmoralesch@outlook.com>
2024-06-18 17:48:14 -0400

    (feat): adding view for crud skeleton

--- arriendo/application/propiedades.py
+++ arriendo/application/propiedades.py
@@ -5,3 +5,12 @@ BASE_HTML_ROUTE = 'propiedades'
 def index(request):
     html_route = 'index.html'
     return render(request, f"{BASE_HTML_ROUTE}/{html_route}", {'propiedades': propiedades_services.get_all()})
+
+def create(request):
+    pass
+
+def update(request, id):
+    pass
+
+def delete(request, id):
+    pass
