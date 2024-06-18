a7b451cea7d4c0838f45a1eef63ea010e1f8eed3
Felipe Morales C <fmoralesch@outlook.com>
2024-06-18 09:48:49 -0400

    (refactor): Removing ..static folder. Moving service's get_all logic to repository

--- arriendo/entities/repository/propiedades.py
+++ arriendo/entities/repository/propiedades.py
@@ -1,7 +1,16 @@
+from typing import List, Dict
 from ..models import Propiedad
 
-def get_all():
-    return Propiedad.objects.all()
+def get_all() -> List[Dict]:
+    return [
+        {
+            "id": p.id,
+            "rol": p.rol,
+            "direccion": p.direccion,
+            "dueno": p.dueno
+        }
+        for p in Propiedad.objects.all()
+    ]
 
 def get_by_id(id :int):
     return Propiedad.objects.get(pk=id)
--- arriendo/services/propiedades.py
+++ arriendo/services/propiedades.py
@@ -2,15 +2,7 @@ from typing import Dict, List
 from ..entities.repository import propiedades as propiedades_repository
 
 def get_all() -> List[Dict]:
-    return [
-        {
-            "id": p.id,
-            "rol": p.rol,
-            "direccion": p.direccion,
-            "dueno": p.dueno
-        }
-        for p in propiedades_repository.get_all()
-    ]
+    return propiedades_repository.get_all()
 
 def get_by_id(id: int) -> Dict:
     pass
